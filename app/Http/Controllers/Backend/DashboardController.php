186dafaab40efc6a13b90ec76cc62f175d66a0e2
danangsptro <danangsaputro300@gmail.com>
2024-06-06 22:23:35 +0700

    update grafik

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,35 +5,68 @@ namespace App\Http\Controllers\Backend;
 use App\hasil_produksi;
 use App\Http\Controllers\Controller;
 use App\problem_produksi;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        // $hasilProduksi = hasil_produksi::all();
-        // $problemProduksi = problem_produksi::all();
+
+        $sales = hasil_produksi::select(
+            DB::raw('YEAR(tanggal_produksi) as year'),
+            DB::raw('MONTH(tanggal_produksi) as month'),
+            DB::raw('SUM(qty_target) as total')
+        )
+            ->groupBy('year', 'month')
+            ->get();
+
+        $labels = [];
+        $data = [];
+
+        foreach ($sales as $sale) {
+            $date = Carbon::create($sale->year, $sale->month, 1);
+            $labels[] =  $date->translatedFormat('F') . ' ' . $sale->year;
+            $data[] = $sale->total;
+        }
 
         $hasilProduksi = [
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'labels' => $labels,
             'datasets' => [
                 [
-                    'label' => 'Hasil Produksi',
+                    'label' => 'Target Hasil Produksi',
                     'backgroundColor' => 'rgba(75, 192, 192, 0.2)',
                     'borderColor' => 'rgba(75, 192, 192, 1)',
-                    'data' => [65, 59, 80, 81, 56, 55, 40],
+                    'data' => $data,
                 ]
             ]
         ];
 
+        $pp = problem_produksi::select(
+            DB::raw('YEAR(tanggal_problem) as year'),
+            DB::raw('MONTH(tanggal_problem) as month'),
+            DB::raw('COUNT(*) as total')
+        )
+            ->groupBy('year', 'month')
+            ->get();
+
+        $labelss = [];
+        $dataa = [];
+
+        foreach ($pp as $sale) {
+            $date = Carbon::create($sale->year, $sale->month, 1);
+            $labelss[] =  $date->translatedFormat('F') . ' ' . $sale->year;
+            $dataa[] = $sale->total;
+        }
+
         $problemProduksi = [
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'labels' => $labelss,
             'datasets' => [
                 [
                     'label' => 'Problem Produksi',
                     'backgroundColor' => 'salmon',
                     'borderColor' => 'rgba(75, 192, 192, 1)',
-                    'data' => [65, 59, 80, 81, 56, 55, 40],
+                    'data' => $dataa,
                 ]
             ]
         ];
